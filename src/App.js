0933e7d31977929f6268e895e8b9e25c808c97c7
diomededavid <david@d2creativedesign.com>
2022-02-23 18:14:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
+  const loading = false;
+
+  if (loading) return <h1>Loading...</h1>;
+  const showComments = true;
+
+  const commentBlock = (
+    <div className='comments'>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      {showComments && commentBlock}
     </div>
   );
 }
