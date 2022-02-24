eec0adca98bb649df04e174e3778f3b9ebff7d04
diomededavid <david@d2creativedesign.com>
2022-02-23 22:19:41 -0700

    Managing State

---
+++ src/Shared/Card.jsx
@@ -0,0 +1,4 @@
+function Card({ children }) {
+  return <div className='card'>{children}</div>;
+}
+export default Card;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react';
+import Card from '../Shared/Card';
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState('This is a feedback item');
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-  };
+function FeedbackItem({ item }) {
   return (
-    <div className='card'>
-      <div className='num-display'>{rating}</div>
-      <div className='text-display'>{text}</div>
-      <button onClick={handleClick}>Click me</button>
-    </div>
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <div className='text-display'>{item.text}</div>
+    </Card>
   );
 }
 
