020b2ded8a8828d10e4bb632c8af63038c20ab65
diomededavid <david@d2creativedesign.com>
2022-02-23 19:44:20 -0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
+import Header from './components/Header';
+
 function App() {
-  const title = 'blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ];
-  const loading = false;
-
-  if (loading) return <h1>Loading...</h1>;
-  const showComments = true;
-
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
+  return (
+    <header style={headerStyles}>
+      <h1>{text}</h1>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0, 0.4)',
+  textColor: '#ff6a85',
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+
+export default Header;
