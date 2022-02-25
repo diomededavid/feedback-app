ef2a87a41ce51c467bc702e8dc2d380eda8f3568
diomededavid <david@d2creativedesign.com>
2022-02-25 14:00:16 -0700

    style based ternary operator

--- src/Shared/Card.jsx
+++ src/Shared/Card.jsx
@@ -1,4 +1,28 @@
+import PropTypes from 'prop-types';
+
 function Card({ children, reverse }) {
+  // using style with ternary operator
+  // return (
+  //     <div
+  //       style={{
+  //         backgroundColor: reverse ? 'rgba(0, 0, 0, 0.4' : '#fff',
+  //         color: reverse ? '#fff' : '#000',
+  //       }}
+  //     >
+  //       {children}
+  //     </div>
+  //   );
+
   return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
 }
+
+Card.defautlProps = {
+  reverse: false,
+};
+
+Card.propTyoes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+};
+
 export default Card;
