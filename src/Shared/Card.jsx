2e2d2402c61f6074d7402b7b98c215bd3660575c
diomededavid <david@d2creativedesign.com>
2022-02-25 10:15:31 -0700

    reverse card

--- src/Shared/Card.jsx
+++ src/Shared/Card.jsx
@@ -1,4 +1,4 @@
-function Card({ children }) {
-  return <div className='card'>{children}</div>;
+function Card({ children, reverse }) {
+  return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
 }
 export default Card;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,7 +2,7 @@ import Card from '../Shared/Card';
 
 function FeedbackItem({ item }) {
   return (
-    <Card>
+    <Card reverse={true}>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
     </Card>
