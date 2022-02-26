7bc92b2da6f63c4b7b1e1c051b9d5f84dee762df
diomededavid <david@d2creativedesign.com>
2022-02-25 19:37:34 -0700

    Lecture 17 Events & Prop Drilling

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  console.log(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} s />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
 import Card from '../Shared/Card';
+import { FaTimes } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <Card reverse={true}>
+    <Card>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   );
